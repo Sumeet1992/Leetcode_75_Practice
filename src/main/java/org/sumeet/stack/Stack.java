7b88aa23ebca6f83383c8b2ddd81a5f0474c6c29
Sumeet Ambastha <sumeet.amb@gmail.com>
2025-01-12 00:27:03 +0530

    Stack basics using static Array

--- src/main/java/org/sumeet/stack/Stack.java
+++ src/main/java/org/sumeet/stack/Stack.java
@@ -1,4 +1,31 @@
 package org.sumeet.stack;
 
 public class Stack {
+
+    int[] stack = new int[5];
+    int top  = 0;
+
+    public void push (int data){
+        stack[top] = data;
+        top++;
+    }
+
+    public int pop(){
+        top--;
+        int pop_data = stack[top];
+        stack[top] = 0;
+        return pop_data;
+    }
+
+    public int peek(){
+        int top_data = stack[top-1];
+        return top_data;
+    }
+
+    public void show(){
+        for(int i : stack){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
---
+++ src/main/java/org/sumeet/stack/StackRunner.java
@@ -0,0 +1,23 @@
+package org.sumeet.stack;
+
+public class StackRunner {
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(7);
+        stack.push(5);
+        stack.push(8);
+        stack.push(2);
+        stack.push(4);
+
+        stack.show();
+
+        System.out.println(stack.peek());
+
+        System.out.println(stack.pop());
+
+        stack.peek();
+        stack.show();
+
+    }
+}
